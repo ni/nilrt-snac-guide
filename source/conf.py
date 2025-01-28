4f630633bdf26cb2416b6af1dac6dab49a7b4433
Mark Silva <mark.silva@emerson.com>
2025-01-28 17:17:14 -0500

    Configuration file required to build PDF from RST source
    Signed-off-by: Mark Silva <mark.silva@emerson.com>

---
+++ source/conf.py
@@ -0,0 +1,85 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'NI Linux RT Security Documentation With SNAC Configuration'
+copyright = '2024-%Y, NI/Emerson'
+author = 'Emerson T&M'
+version = '1.1'
+release = '1.1.0'
+nist_rev = '3'  # NIST 800-171 Revision number
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+primary_domain = None
+source_encoding = 'utf-8'
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+}
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+# -- Options for LaTeX/PDF output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-latex-pdf-output
+latex_engine = 'xelatex'
+
+latex_documents = [
+    ('index', 'SNAC.tex', 'NI Linux RT Security Documentation With SNAC Configuration', 'NI/Emerson', 'manual', False),
+]
+
+# Needed so file is copied to build location
+latex_logo = 'media/logo.png'
+
+latex_elements = {
+    # The paper size ('letterpaper' or 'a4paper').
+    'papersize': 'letterpaper',
+
+    # The font size ('10pt', '11pt' or '12pt').
+    'pointsize': '12pt',
+
+    # Additional stuff for the LaTeX preamble.
+    # \setcounter{secnumdepth}{0} Disables section numbering
+    'preamble': r'''
+        \usepackage{helvet}
+        \usepackage[defaultsans]{lato}
+        \usepackage{inconsolata}
+        \usepackage{lscape}
+        \usepackage{scalefnt }
+        \setlength{\headheight}{14.5pt}
+        \setcounter{secnumdepth}{0}
+    ''',
+
+    # Custom title page
+    'maketitle': r'''
+        \begin{titlepage}
+            \centering
+            \vspace*{2cm}
+            {\Huge \textbf{%s}\par}
+            \vspace{1.5cm}
+            {\Large \textbf{Based on NIST 800-171 Rev%s}\par}
+            \vspace{1.5cm}
+            {\Large \textbf{%s}\par}
+            \vspace{1.5cm}
+            {\Large Release: %s\par}
+            \vfill
+            {\Large \today\par}
+            \vspace*{2cm}
+            \includegraphics[width=0.25\textwidth]{logo.png}
+        \end{titlepage}
+    ''' % (project, nist_rev, author, release)
+}
