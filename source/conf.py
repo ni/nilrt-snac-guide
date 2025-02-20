d6fe704f8d237d91bce06acad860e1717f1fe185
Richard Tollerton <rich.tollerton@ni.com>
2025-02-19 19:28:39 -0600

    conf.py: reduce pointsize from 12pt to 11pt
    12pt is significantly larger than comparable documents, notably NIST standards
    and past NI manuals in PDF format. It is alone responsible for quite a few
    typesetting bugs due to running out of space. Reducing it to 11pt significantly
    improves typesetting quality at negligible or perhaps even positive change to
    legibility.
    
    That said, this caused the already-scaled-down Appendix 2 table to be too small
    to read, so its \scalefont has been adjusted appropriately.
    
    Signed-off-by: Richard Tollerton <rich.tollerton@ni.com>

--- source/conf.py
+++ source/conf.py
@@ -50,7 +50,7 @@ latex_elements = {
     'papersize': 'letterpaper',
 
     # The font size ('10pt', '11pt' or '12pt').
-    'pointsize': '12pt',
+    'pointsize': '11pt',
 
     # Additional stuff for the LaTeX preamble.
     # \setcounter{secnumdepth}{0} Disables section numbering
